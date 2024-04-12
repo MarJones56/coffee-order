b9c5b091202e61bee70d7316345673e4c655e26a
MarJones56 <marjone@iu.edu>
2024-04-11 20:34:55 -0400

    h8

---
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package edu.iu.habahram.coffeeorder.controllers;
+
+import edu.iu.habahram.coffeeorder.repository.CustomerRepository;
+import edu.iu.habahram.coffeeorder.model.Customer;
+import edu.iu.habahram.coffeeorder.security.TokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    CustomerRepository customerRepository;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    TokenService tokenService,
+                                    CustomerRepository customerRepository) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.customerRepository = customerRepository;
+    }
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.username()
+                                , customer.password()));
+
+        return tokenService.generateToken(authentication);
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
