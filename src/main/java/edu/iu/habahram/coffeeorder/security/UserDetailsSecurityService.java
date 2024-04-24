10422e9299ad8eaf24c2b2fe17a922f6cd37cb5e
MarJones56 <marjone@iu.edu>
2024-04-24 17:25:49 -0400

    hw9

--- src/main/java/edu/iu/habahram/coffeeorder/security/UserDetailsSecurityService.java
+++ src/main/java/edu/iu/habahram/coffeeorder/security/UserDetailsSecurityService.java
@@ -8,23 +8,27 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserDetailsSecurityService implements UserDetailsService {
+public class UserDetailsSecurityService implements
+        UserDetailsService {
     CustomerRepository customerRepository;
 
-    public UserDetailsSecurityService (CustomerRepository customerRepository) {
+    public UserDetailsSecurityService(CustomerRepository
+                                              customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @Override
-    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
         try {
-            Customer customer = customerRepository.findByUsername(username);
-            if (customer == null) {
+            Customer customer =
+                    customerRepository.findByUsername(username);
+            if(customer == null) {
                 throw new UsernameNotFoundException("");
             }
             return User
                     .withUsername(username)
-                    .password(customer.password())
+                    .password(customer.getPassword())
                     .build();
         } catch (Exception e) {
             throw new RuntimeException(e);
